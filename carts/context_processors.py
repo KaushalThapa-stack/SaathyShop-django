6e16c4d5e263e76a386d00dc1507ea795474b32c
Kaushal Thapa <misterio3669@gmail.com>
2025-07-26 12:58:49 +0545

    handeld cart and checkout page for logged in users

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -4,23 +4,18 @@ from .views import _cart_id
 
 
 def counter(request):
-    print("Counter function called")  # Debug print
     cart_count = 0
     if 'admin' in request.path:
         return {}
     else:
         try:
-            cart_id = _cart_id(request)
-            print(f"Cart ID: {cart_id}")  # Debug print
-            cart = Cart.objects.get(cart_id=cart_id)
-            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+            cart = Cart.objects.filter(cart_id=_cart_id(request))
+            if request.user.is_authenticated:
+                cart_items = CartItem.objects.all().filter(user=request.user)
+            else:
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
                 cart_count += cart_item.quantity
-            print(f"Cart count: {cart_count}")  # Debug print
         except Cart.DoesNotExist:
             cart_count = 0
-            print("Cart does not exist")  # Debug print
-        except Exception as e:
-            cart_count = 0
-            print(f"Exception: {e}")  # Debug print
     return dict(cart_count=cart_count)
